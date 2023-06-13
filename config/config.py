80c86653d52cde472769dec14621c3368c451501
Anastasiya Myasnikova <nastabutcher.myasn@gmail.com>
2023-06-13 15:26:46 +0500

    commit 12

--- config/config.py
+++ config/config.py
@@ -10,10 +10,14 @@
 # DB_PORT=os.environ.get('DB_PORT')
 
 import os
+os.environ ["DB_NAME"] = 'tsan_actions'
+os.environ ["DB_USER"] = 'postgres'
+os.environ ["DB_PASSWORD"] = 'Nasta678!'
+os.environ ["DB_HOST"] = 'localhost'
+os.environ ["DB_PORT"] = '5432'
 
 DB_NAME=os.environ['DB_NAME']
 DB_USER=os.environ['DB_USER']
 DB_PASSWORD=os.environ['DB_PASSWORD']
 DB_HOST=os.environ['DB_HOST']
 DB_PORT=os.environ['DB_PORT']
-
